acd18cb3fadee6692a36a553a0e422c90848dfc3
GOWRISANKARHARI <2k21ece032@kiot.ac.in>
2025-04-02 20:26:05 +0530

    newwwwwwww

--- cucumber_pro2/src/test/java/runner/runn.java
+++ cucumber_pro2/src/test/java/runner/runn.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-    plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"},
+    plugin = {"pretty", "json:target/cucumber-reports1/Cucumber1.json"},
     features = "src/test/resources/com/feature/loginpage.feature",
     glue = "cucumber_pro2",
     monochrome = true
